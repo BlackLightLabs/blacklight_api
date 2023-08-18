4f9403e9991d716ecf180db7b21bfc790a362fa6
ctagard <ctagard19@gmail.com>
2023-08-18 11:55:03 -0600

    [feat] Added login/register screen
    Utilized DjangoREST to build out the login/register views for the user. Added views in the views folder, in the future we will need to add different "apps" for different things (one for user and so on).

--- api/models.py
+++ api/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 
 # Create your models here.
 class BlacklightNeuralNetworkRun(models.Model):
@@ -12,3 +12,32 @@ class BlacklightNeuralNetworkRun(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Datasets(models.Model): 
+    """Model representing a dataset."""
+    id = models.AutoField(primary_key=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    description = models.CharField(max_length=200)
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_modified = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=20, default='pending')
+    file_path = models.CharField(max_length=255, blank=True, null=True)
+    result = models.TextField(blank=True, null=True)
+    def __str__(self):
+        return self.name
+
+
+class Analysis(models.Model): 
+    """Model representing a specific analysis that is run on a dataset."""
+    id = models.AutoField(primary_key=True)
+    dataset_id = models.ForeignKey(Datasets, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    analysis_type = models.CharField(max_length=200)
+    flags = models.CharField(max_length=200)
+    def __str__(self):
+        return self.name
+
+
+
--- api/tasks.py
+++ api/tasks.py
@@ -1,32 +1,6 @@
 from celery import shared_task
-from .models import BlacklightNeuralNetworkRun
-import os
-from django.conf import settings
-from blacklight.autoML import FeedForward
-
 
 @shared_task
-def fit_neural_network(nn_id, X_train, y_train, X_test, y_test, options):
-    nn = BlacklightNeuralNetworkRun.objects.get(id=nn_id)
-    nn.status = "in progress"
-    nn.save()
-
-    # Initialize your custom FeedForward class
-    ff = FeedForward(
-        number_of_individuals=options["number_of_individuals"],
-        num_parents_mating=options["num_parents_mating"],
-        death_percentage=options["death_percentage"],
-        number_of_generations=options["number_of_generations"],
-        options=options["options"]
-    )
-
-    ff.fit(X_train, y_train, X_test, y_test)
-
-    # Save the model to a file
-    model_filename = f"model_{nn_id}.h5"
-    model_path = os.path.join(settings.MEDIA_ROOT, model_filename)
-    ff.model.save(model_path)
-
-    nn.status = "completed"
-    nn.result = "your_result_representation"  # Serialize the result as needed
-    nn.save()
+def print_hello():
+    print('Hello World!')
+    return None
