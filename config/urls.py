b587de3b14d459f5e357f0f6be517bf172808920
ctagard <ctagard19@gmail.com>
2023-08-19 21:00:32 -0600

    [feat] refactored users to have thier own Django App

--- config/urls.py
+++ config/urls.py
@@ -18,7 +18,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path('api/users/', include('api.urls.user_urls')),
+    path('api/users/', include('users.urls')),
     path('api/datasets/', include('datasets.urls')),
     path('api/analyses/', include('analysis.urls')),
 ]
